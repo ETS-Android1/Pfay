1a6dbb70e8740aa974f19d098af44f36aee968cf
Pham Van Long Phuoc <phuoc002@e.ntu.edu.sg>
2021-10-16 21:23:28 +0800

    Phuoc dep zai vua debug

---
+++ D2Go/ObjectDetection/app/src/main/java/org/pytorch/demo/objectdetection/CameraPreview.java
@@ -0,0 +1,109 @@
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.media.Image;
+import android.media.ImageReader;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+
+import android.util.Size;
+import android.view.Surface;
+
+import org.pytorch.demo.objectdetection.R;
+
+
+public class CameraPreview extends AppCompatActivity implements ImageReader.OnImageAvailableListener {
+    Handler handler;
+    private int sensorOrientation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        handler = new Handler();
+
+        //TODO ask for camera permissions
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.CAMERA}, 121);
+        } else {
+            //TODO show live camera footage
+            setFragment();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //TODO show live camera footage
+        if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            setFragment();
+        } else {
+            finish();
+        }
+    }
+
+    //TODO fragment which show llive footage from camera
+    int previewHeight = 0, previewWidth = 0;
+
+    protected void setFragment() {
+        final CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        String cameraId = null;
+        try {
+            cameraId = manager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+        Fragment fragment;
+        org.pytorch.demo.objectdetection.imageclassificationlivefeed.CameraConnectionFragment camera2Fragment =
+                org.pytorch.demo.objectdetection.imageclassificationlivefeed.CameraConnectionFragment.newInstance(
+                        new org.pytorch.demo.objectdetection.imageclassificationlivefeed.CameraConnectionFragment.ConnectionCallback() {
+                            @Override
+                            public void onPreviewSizeChosen(final Size size, final int rotation) {
+                                previewHeight = size.getHeight();
+                                previewWidth = size.getWidth();
+                                sensorOrientation = rotation - getScreenOrientation();
+                            }
+                        },
+                        this,
+                        R.layout.camera_fragment,
+                        new Size(640, 480));
+
+        camera2Fragment.setCamera(cameraId);
+        fragment = camera2Fragment;
+
+        getFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+    }
+
+    protected int getScreenOrientation() {
+        switch (getWindowManager().getDefaultDisplay().getRotation()) {
+            case Surface.ROTATION_270:
+                return 270;
+            case Surface.ROTATION_180:
+                return 180;
+            case Surface.ROTATION_90:
+                return 90;
+            default:
+                return 0;
+        }
+    }
+
+
+    //TODO getting frames of live camera footage and passing them to model
+
+    @Override
+    public void onImageAvailable(ImageReader reader) {
+        final Image image = reader.acquireLatestImage();
+        image.close();
+    }
+}
